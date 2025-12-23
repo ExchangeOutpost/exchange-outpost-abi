a4684aff491075a001d788252a943911197f8d78
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-12-23 23:23:01 +0100

    feat: re-export extism_pdk for use in wasm modules

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,6 @@ pub use candle::Candle;
 pub use fin_data::FunctionArgs;
 pub use notifications::schedule_email;
 pub use notifications::schedule_webhook;
+
+
+pub use extism_pdk;  // re-exporting extism_pdk so that it can be used in the wasm modules
